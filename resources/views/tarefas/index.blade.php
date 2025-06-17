04b1910070ea2abb619e16223d5ebda8b7e3cc48
ximass <mateus.schmitz7@hotmail.com>
2025-06-17 19:31:01 -0300

    teste

--- resources/views/tarefas/index.blade.php
+++ resources/views/tarefas/index.blade.php
@@ -2,7 +2,7 @@
 <html lang="pt">
 <head>
     <meta charset="UTF-8">
-    <title>Listagem de Tarefas</title>
+    <title>Listagem de Tarefas 1</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
